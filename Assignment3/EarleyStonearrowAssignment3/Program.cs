9ebb6dd05d1dc18eab8a3b4df3cadf992cdebf97
stonearrowschool <skyler.earleystonearrow@trojans.dsu.edu>
2024-06-12 11:40:17 -0600

    Assignment 3 uploaded

---
+++ Assignment3/EarleyStonearrowAssignment3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EarleyStonearrowAssignment3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Future upgrades = attempt to utilize Dictionary(int, List<string>)
+            // Dictionary(Student ID, Student info in a list)
+            // Declare lists of all student values
+            List<string> studentIDs = new List<string>();
+            List<string> studentNames = new List<string>();
+            List<string> studentEmails = new List<string>();
+            List<string> phoneNumbers = new List<string>();
+            List<string> studentMajors = new List<string>();
+            List<string> startDates = new List<string>();
+            List<string> estGradDates = new List<string>();
+            List<string> mailAddresses = new List<string>();
+            List<string> states = new List<string>();
+            List<string> countries = new List<string>();
+            List<bool> enableHidden = new List<bool>();
+            List<string> activityLog = new List<string>();
+            // Declare local variables 
+            string name, major, email, phoneNumber, hiddenInput, startDate, estGradDate,
+                mailAddress, state, country, log = "";
+            bool hidden;
+            int entries;
+
+            // Ask user how many entries to input for loop
+            Console.WriteLine("How many entries do you wish to enter?");
+            entries = int.Parse(Console.ReadLine());
+
+            // Loop through user creation
+            for(int i = 0; i < entries; i++)
+            {
+                Console.WriteLine("Name: ");
+                name = Console.ReadLine();
+                Console.WriteLine("Major: ");
+                major = Console.ReadLine();
+                Console.WriteLine("Email: ");
+                email = Console.ReadLine();
+                Console.WriteLine("Phone Numbers: ");
+                phoneNumber = Console.ReadLine();
+                Console.WriteLine("Start Date: (MM/DD/YYYY)");
+                startDate = Console.ReadLine();
+                Console.WriteLine("Anticipated Graduation Date: (MM/DD/YYYY)");
+                estGradDate = Console.ReadLine();
+                Console.WriteLine("Mailing Address: ");
+                mailAddress = Console.ReadLine();
+                Console.WriteLine("State/Province/Territory: ");
+                state = Console.ReadLine();
+                Console.WriteLine("Country: ");
+                country = Console.ReadLine();
+                Console.WriteLine("Enable hidden? Y/N: ");
+                hiddenInput = Console.ReadLine();
+                if (hiddenInput == "Y" || hiddenInput == "y")
+                {
+                    hidden = true;
+                }
+                else
+                {
+                    hidden = false;
+                }
+                Console.WriteLine(" ");
+
+                // Send values to newStudent (Student.cs)
+                Student newStudent = new Student(i+1, name, email, phoneNumber, major, startDate, estGradDate, 
+                    mailAddress, state, country, hidden, log);
+                // Add values to lists
+                studentIDs.Add(newStudent.studentID.ToString());
+                studentNames.Add(newStudent.Name);
+                studentMajors.Add(newStudent.Major);
+                studentEmails.Add(newStudent.Email);
+                phoneNumbers.Add(newStudent.PhoneNumber);
+                startDates.Add(newStudent.StartDate);
+                estGradDates.Add(newStudent.EstGradDate);
+                mailAddresses.Add(newStudent.MailAddress);
+                states.Add(newStudent.State);
+                countries.Add(newStudent.Country);
+                enableHidden.Add(newStudent.hidden);
+                activityLog.Add(newStudent.log);
+            }
+            
+            // Loop through the number of ID's (# of students) and output a formatted output
+            for (int i = 0; i < studentIDs.Count; i++)
+            {
+                Console.WriteLine($"ID: \t\t\t\t{studentIDs[i]}");
+                Console.WriteLine($"Name: \t\t\t\t{studentNames[i]}");
+                Console.WriteLine($"Email: \t\t\t\t{studentEmails[i]}");
+                // if the hidden value is set to false, display the below info
+                if (enableHidden[i] != true)
+                {
+                    Console.WriteLine($"Phone Number: \t\t{phoneNumbers[i]}");
+                    Console.WriteLine($"Major: \t\t\t\t{studentMajors[i]}");
+                    Console.WriteLine($"Start Date: \t\t\t{startDates[i]}");
+                    Console.WriteLine($"Est. Graduation Date: \t\t{estGradDates[i]}");
+                    Console.WriteLine($"Mailing Address: \t\t{mailAddresses[i]}");
+                    Console.WriteLine($"State/Territory/Province: \t{states[i]}");
+                    Console.WriteLine($"Country: \t\t\t{countries[i]}");
+                }
+                // separate user output
+                Console.WriteLine(" ");
+            }
+            // format an activity log
+            Console.WriteLine($"Activity Log");
+            Console.WriteLine("------------");
+            // loop through number of students and output activity log list
+            for(int i = 0; i < studentIDs.Count; i++)
+            {
+                Console.WriteLine(activityLog[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment3/EarleyStonearrowAssignment3/Student.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace EarleyStonearrowAssignment3
+{
+    internal class Student
+    {
+        // Declare variables, including a readonly
+        public readonly int studentID;
+        public string name, major, startDate, estGradDate, mailAddress, state, country, 
+            email, phoneNumber, log;
+        public bool hidden;
+
+        // Parameterized Constructor
+        public Student(int pStudentID, string pName, string pEmail, string pPhoneNumber, 
+            string pMajor, string pStartDate, string pEstGradDate, string pMailAddress, 
+            string pState,  string pCountry, bool pHidden, string pLog)
+        {
+            studentID = pStudentID;
+            hidden = pHidden;
+            log = pLog;
+            Name = pName;
+            Email = pEmail;
+            PhoneNumber = pPhoneNumber;
+            Major = pMajor;
+            StartDate = pStartDate;
+            EstGradDate = pEstGradDate;
+            MailAddress = pMailAddress;
+            State = pState;
+            Country = pCountry;
+        }
+        // Properties in alphabetical order
+        public string Country
+        {
+            get { return country; }
+            set
+            {
+                country = value;
+                log += $"Country of {country} added to {name}\n";
+            }
+        }
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                email = value;
+                log += $"Email of {email} added to {name}\n";
+            }
+        }
+
+        public string EstGradDate
+        {
+            get
+            {
+                return estGradDate;
+            }
+            set
+            {
+                // Check date/time format is matching one of these patterns
+                var formats = new[] { "M/d/yyyy", "M/dd/yyyy", "MM/d/yyyy", "MM/dd/yyyy" };
+                DateTime dt;
+                if (DateTime.TryParseExact(value, formats, null, DateTimeStyles.None, out dt))
+                {
+                    estGradDate = value;
+                    log += $"Est. Graduation Date of {estGradDate} added to {name}\n";
+                }
+                else
+                {
+                    estGradDate = null;
+                }
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                log += $"Name of {name} added to Student ID {studentID}\n";
+            }
+
+        }
+        
+        public string MailAddress
+        {
+            get
+            {
+                return mailAddress;
+            }
+            set
+            {
+                mailAddress = value;
+                log += $"Mailing Address of {mailAddress} added to {name}\n";
+            }
+        }
+        public string Major
+        {
+            get
+            {
+                return major;
+            }
+            set
+            {
+                major = value;
+                log += $"Major of {major} added to {name}\n";
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set
+            {
+                // set phone regex pattern
+                string pattern = @"[0-9]{10}";
+                // strip out common special characters used in phone numbers to get just digits
+                string strippedVal = value.Replace("-", "");
+                strippedVal = strippedVal.Replace("(", "");
+                strippedVal = strippedVal.Replace(")", "");
+                strippedVal = strippedVal.Replace(".", "");
+                strippedVal = strippedVal.Replace(" ", "");
+
+                //match the just digits to the regex
+                Match match = Regex.Match(strippedVal, pattern, RegexOptions.IgnoreCase);
+                if (match.Success && strippedVal.Length < 11)
+                {
+                    phoneNumber = value;
+                    log += $"Phone Number of {phoneNumber} added to {name}\n";
+                }
+                else
+                {
+                    phoneNumber = null;
+                }
+            }
+        }
+
+        public string StartDate
+        {
+            get
+            {
+                return startDate;
+            }
+            set
+            {
+                // Check date/time format is matching one of these patterns
+                var formats = new[] { "M/d/yyyy", "M/dd/yyyy", "MM/d/yyyy", "MM/dd/yyyy" };
+                DateTime dt;
+                if (DateTime.TryParseExact(value, formats, null, DateTimeStyles.None, out dt))
+                {
+                    startDate = value;
+                    log += $"Start Date of {startDate} added to {name}\n";
+                }
+                else
+                {
+                    startDate = null;
+                }
+            }
+        }
+
+        public string State
+        {
+            get { return state; }
+            set
+            {
+                state = value;
+                log += $"State of {state} added to {name}\n";
+            }
+        }
+    }
+}
